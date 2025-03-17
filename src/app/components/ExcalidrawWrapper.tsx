61ea0d127b435483bf94b054512bcf1c20f641bc
rmmir <radumiron12@gmail.com>
2025-03-17 11:35:34 +0200

    Initial commit from Create Next App

---
+++ src/app/components/ExcalidrawWrapper.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Excalidraw } from "@excalidraw/excalidraw";
+
+import "@excalidraw/excalidraw/index.css";
+
+const ExcalidrawWrapper: React.FC = () => {
+  return (
+    <div style={{ border: '4px solid red', height: "500px", width:"500px"}}>
+      <Excalidraw />
+    </div>
+  );
+};
+
+export default ExcalidrawWrapper;
