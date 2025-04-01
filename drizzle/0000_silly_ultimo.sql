5a0630fb6e9f0eae44797fba2e882fb731c06019
rmmir <radumiron12@gmail.com>
2025-04-01 22:51:49 +0300

    chore: update gitignore + migrations cleanup

--- drizzle/0000_silly_ultimo.sql
+++ drizzle/0000_silly_ultimo.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `user` (
-	`id` text PRIMARY KEY DEFAULT 'e80c794c-a1d6-4674-a1b2-592231101117' NOT NULL,
+	`id` text PRIMARY KEY DEFAULT '05c28ba3-8e1f-4e7e-834d-0ec183c6ab18' NOT NULL,
 	`name` text NOT NULL,
 	`email` text NOT NULL,
 	`password` text NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE `user` (
 --> statement-breakpoint
 CREATE UNIQUE INDEX `user_email_unique` ON `user` (`email`);--> statement-breakpoint
 CREATE TABLE `whiteboard` (
-	`id` text PRIMARY KEY DEFAULT 'e514171d-f6e5-4737-bc14-a77cf5bde9fb' NOT NULL,
+	`id` text PRIMARY KEY DEFAULT 'e10fb7b2-773f-4a4b-843c-29f962f0db62' NOT NULL,
 	`name` text NOT NULL,
 	`elements` text NOT NULL,
 	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
